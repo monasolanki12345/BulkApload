9c24dadfaa5caff4fd5e927dcbd3217fe4eb0320
monasolanki12345 <monasolanki2021@outlook.com>
2022-06-30 18:57:16 +0530

    views routes.rb migration schema.rb files added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  # get 'items_imports/new'
+  # get 'items_imports/create'
+  # get 'items/index'
+  # get 'items/new'
+  # get 'items/index'
+  # get 'items/new'
+
+  resources :items
+  resources :items_imports, only: [:new, :create]
+
+  root to: 'items_imports#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
